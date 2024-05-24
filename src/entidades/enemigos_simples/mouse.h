0859e2cd68432b531c9dd58019a08f68b096df56
Jose Barquero Bonilla <jos.barquero@estudiantec.cr>
2024-05-24 10:49:59 -0600

    Enemigo Raton (Terminado)

--- src/entidades/enemigos_simples/mouse.h
+++ src/entidades/enemigos_simples/mouse.h
@@ -3,6 +3,8 @@
 
 #include "../../../godot-cpp/gen/include/godot_cpp/classes/character_body2d.hpp"
 #include "../../../godot-cpp/gen/include/godot_cpp/classes/animated_sprite2d.hpp"
+#include "../../../godot-cpp/gen/include/godot_cpp/classes/timer.hpp"
+#include <random>
 
 namespace godot {
     class Raton : public CharacterBody2D {
@@ -11,12 +13,16 @@ namespace godot {
     private:
 
         int vidas;
+        double velocidad;
 
         AnimatedSprite2D* animated_run;
         AnimatedSprite2D* animated_run_left;
 
         Vector2 current_direction;
         Vector2 last_horizontal_direction;
+
+        Timer* position_timer;
+        std::mt19937 rng; // Motor de números aleatorios
 
     protected:
         static void _bind_methods();
@@ -25,9 +31,20 @@ namespace godot {
         Raton();
         ~Raton();
 
+        int get_vidas() const;
+        void set_vidas(int p_vidas);
+
+        double get_velocidad() const;
+        void set_velocidad(double p_velocidad);
+
+
+
         void _ready() override;
         void _process(double delta) override;
         void update_animations();
+
+        void _on_position_timer_timeout();
+        void choose_new_direction();
 
     };
 }
